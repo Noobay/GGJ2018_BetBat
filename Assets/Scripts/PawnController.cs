95340e0b7b6c35393187d228404b12e4d5cda9b8
noobay <shadowswatcher@gmail.com>
2018-01-29 07:33:26 +0200

    added cross-platform support for inputs

--- Assets/Scripts/PawnController.cs
+++ Assets/Scripts/PawnController.cs
@@ -5,17 +5,20 @@ public class PawnController : MonoBehaviour {
 
     private static readonly string DEAD_ANIMATION_KEY = "dead";
     private static readonly string DIE_BOOL_KEY = "die";
-    private static readonly string DEATH_STATE_KEY = "death";
 
     [SerializeField]
     private Transform _spawnTransform;
 
     private RaycastHit2D _hit;
-    private Vector2 _firstTouchPosition;
+    private Vector3 _firstTouchPosition;
     private bool _hitByLastTouchBegin = false;
 
     [SerializeField]
-    private float _swipeStrength = 10f;
+    private float _swipeMaxStrength = 10f;
+    [SerializeField]
+    private float _swipeStrengthMutliplier = 1f;
+    [SerializeField]
+    private float _swipeTouchStrengthMultiplier = 0.2f;
 
     private Animator _animator;
 
@@ -26,6 +29,10 @@ public class PawnController : MonoBehaviour {
 
     public void Update()
     {
+
+#if UNITY_ANDROID || UNITY_IOS
+
+        // this should be managed from an input manager of somesort
         if (Input.touchCount == 0)
         {
             return;
@@ -38,9 +45,9 @@ public class PawnController : MonoBehaviour {
             Vector3 touch_origin = Camera.main.ScreenToWorldPoint(currentTouch.position);
             RaycastHit2D hit = Physics2D.CircleCast(touch_origin, 3, Camera.main.transform.forward);
 
-            if (hit && hit.collider & (hit.collider.gameObject.GetInstanceID() == gameObject.GetInstanceID()))
+            if (hit && hit.collider && (hit.collider.gameObject.GetInstanceID() == gameObject.GetInstanceID()))
             {
-                _swipeStarted(currentTouch);
+                _swipeStarted(currentTouch.position);
                 _hitByLastTouchBegin = true;
             } else
             {
@@ -49,12 +56,33 @@ public class PawnController : MonoBehaviour {
         }
         else if (_hitByLastTouchBegin && currentTouch.phase == TouchPhase.Ended)
         {
-            _swipeEnded(currentTouch);
+            _swipeEnded(currentTouch.position);
             _hitByLastTouchBegin = false;
         }
-
+#endif
     }
 
+#if UNITY_EDITOR_WIN || UNITY_WSA || UNITY_WEBGL
+    private void OnMouseDown()
+    {
+        Vector3 currentPos = Input.mousePosition;
+        // Construct a ray from the current mouse coordinates
+        Vector3 touch_origin = Camera.main.ScreenToWorldPoint(currentPos);
+        RaycastHit2D hit = Physics2D.CircleCast(touch_origin, 3, Camera.main.transform.forward);
+
+
+        if (hit && hit.collider && (hit.collider.gameObject.GetInstanceID() == gameObject.GetInstanceID()))
+        {
+            _swipeStarted(currentPos);
+            _hitByLastTouchBegin = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        _swipeEnded(Input.mousePosition);
+    }
+#endif 
     private IEnumerator _waitForStateAndDie(string animation_name)
     {
         do
@@ -73,7 +101,6 @@ public class PawnController : MonoBehaviour {
 
     public void Revive()
     {
-        _animator.SetBool(DEATH_STATE_KEY, false);
         _animator.SetBool(DIE_BOOL_KEY, false);
 
         transform.position = (Vector2)_spawnTransform.position;
@@ -85,21 +112,20 @@ public class PawnController : MonoBehaviour {
         LivesManager.enqueueDeadBat(this);
     }
 
-    private void _swipeStarted(Touch touch)
+    private void _swipeStarted(Vector3 position)
     {
-        if (touch.deltaPosition.sqrMagnitude == 0)
-        {
-            return;
-        }
- 
-        _firstTouchPosition = touch .deltaPosition;
+        _firstTouchPosition = position;
     }
 
-    private void _swipeEnded(Touch touch)
+    private void _swipeEnded(Vector3 position)
     {
-        Vector2 touchVector = ((touch.deltaPosition + _firstTouchPosition) / 2).normalized;
-        GetComponent<Rigidbody2D>().AddForce(touchVector * _swipeStrength, ForceMode2D.Impulse);
-        Debug.Log(touchVector);
-        Debug.Log(_firstTouchPosition + " and: " + touch.position);
+        Vector2 swipeVector = -1 * (_firstTouchPosition - position);
+
+        Vector2 normalizedSwipe = swipeVector.normalized;
+        float swipeMagnitude = Mathf.Clamp(swipeVector.magnitude, 0f, _swipeMaxStrength);
+#if UNITY_ANDROID || UNITY_IOS
+        swipeMagnitude *= _swipeTouchStrengthMultiplier;
+#endif
+        GetComponent<Rigidbody2D>().AddForce(swipeVector * swipeMagnitude * _swipeStrengthMutliplier, ForceMode2D.Impulse);
     }
 }
