57fa537fb7ae53322651291bbfc065e1f055f26f
noobay <shadowswatcher@gmail.com>
2018-01-29 07:33:00 +0200

    added Tags and Layers for collision handling. Minor refactoring

--- Assets/BigBatController.cs
+++ Assets/BigBatController.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class BigBatController : MonoBehaviour {
     // Both this class and PawnController should have an interface for a basic BatController
 
-    private static string DIE_BOOl_KEY = "die";
+    private static string DIE_BOOL_KEY = "die";
 
     private Animator _animator;
 
@@ -19,12 +19,12 @@ public class BigBatController : MonoBehaviour {
 	}
 
     public void OnCollisionEnter2D(Collision2D collision) {
-        
+
     }
 
     public void ActDeath()
     {
-        _animator.SetBool(DIE_BOOl_KEY, true);
+        _animator.SetBool(DIE_BOOL_KEY, true);
 
         // TODO: LOSE GAME, this shouldn't be here
         SceneManager.LoadScene(0);
